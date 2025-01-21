11d46a5a41277ed0931428fed26f3cf5b64479dd
Misael <misaelcsauceda@gmail.com>
2025-01-21 14:37:20 -0300

    minor fixes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use redis::{AsyncCommands, Client, Msg};
+use redis::Client;
 use dotenv::dotenv;
+use futures_util::StreamExt;
 use std::env;
 use std::fs::OpenOptions;
 use std::io::Write;
@@ -12,33 +13,33 @@ async fn main() -> redis::RedisResult<()> {
     let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set");
     let channel_name = env::var("CHANNEL_NAME").expect("CHANNEL_NAME must be set");
 
-    // Connect to Redis
+    // Connect to Redis and subscribe to the channel
     let client = Client::open(redis_url)?;
-    let mut connection = client.get_async_connection().await?;
-    let mut pubsub = connection.as_pubsub();
-    pubsub.subscribe(channel_name).await?;
+    let mut pubsub = client.get_async_pubsub().await?;
+    pubsub.subscribe(&channel_name).await?;
 
-    println!("Subscribed to the channel.");
+    println!("Subscribed to the channel: {}", channel_name);
 
-    loop {
-        if let Some(msg) = pubsub.on_message().next().await {
-            let payload: String = msg.get_payload().expect("Payload should be valid UTF-8");
-            println!("Received: {}", payload);
+    // Message handling loop
+    while let Some(msg) = pubsub.on_message().next().await {
+        let payload: String = msg.get_payload()?;
+        println!("Received: {}", payload);
 
-            // Spawn a new task for each publication
-            task::spawn(handle_message(payload));
-        }
+        // Spawn a new task for each publication
+        task::spawn(async move {
+            handle_message(payload).await
+        });
     }
+
+    Ok(())
 }
 
-async fn handle_message(payload: String) {
-    log_to_file(format!("Payload: {}", payload)).expect("Failed to write to log");
+async fn handle_message(payload: String) -> std::io::Result<()> {
+    log_to_file(format!("PROCESSING MESSAGE: {}", payload))?;
 
-    // Wait another second
-    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    // TODO:: publish a message saying "Hi stranger" to the same channel
 
-    // Log "finished"
-    log_to_file("Finished".to_string()).expect("Failed to write to log");
+    Ok(())
 }
 
 fn log_to_file(content: String) -> std::io::Result<()> {
